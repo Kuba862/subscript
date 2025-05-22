1a984ab186009e3610c3122b8cd36cacfb115f0a
jakubrejch <jrejch@citizengo.net>
2025-05-22 11:30:43 +0200

    add possibility to assign task to the user

---
+++ server/migrations/20250522090915_add_assigne_to_task.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.table('todos', function(table) {
+    table.integer('assignee_id').unsigned().references('id').inTable('users').onDelete('SET NULL');
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.table("todos", function(table) {
+    table.dropColumn("assignee_id");
+  });
+};
